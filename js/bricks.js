9b8421f0122baf250fa34669c1670a6cc51f9904
juansemprun <33623712+juansemprun@users.noreply.github.com>
2020-08-29 15:37:24 +0200

    Add files via upload

--- js/bricks.js
+++ js/bricks.js
@@ -1,9 +1,23 @@
 class Brick {
 
-    constructor(ctx, canvasSize) {
+    constructor(ctx, brickPosX, brickPosY, brickSizeW, brickSizeH, canvasSize) {
 
         this.ctx = ctx
         this.canvasSize = canvasSize
+        this.brickPos = {
+            x: brickPosX,
+            y: brickPosY
+        }
+        this.brickSize = {
+            w: brickSizeW,
+            h: brickSizeH
+        }
+
+    }
+
+    draw() {
+
+        this.ctx.fillRect(this.brickPos.x, this.brickPos.y, this.brickSize.w, this.brickSize.h)
 
     }
 
